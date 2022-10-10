e60a6e048cd6e4b6b1551de8c34a95d86e37e3e4
Gustavo Rodrigues <gustavo.rodrigues@sequor.com.br>
2022-10-10 08:26:55 -0300

    correction exercise 7

--- 7/function.js
+++ 7/function.js
@@ -5,18 +5,19 @@ situação em relação ao alistamento militar.
 alistamento. */
 
 let anoNascimento = prompt("Digite o ano do seu nascimento:");
+let data = new Date();
+let anoAtual = data.getFullYear();
+let idade = anoAtual - anoNascimento;
+let maiorDezoito = 18 - idade;
+let menorDezeoito = idade - 18;
 
 function alistamentoMilitar() {
-    let anoAtual = 2022;
-    let idade = anoAtual - anoNascimento;
-    let maiorDezoito = 18 - idade;
-    let menorDezeoito = idade - 18;
     if (idade == 18) {
         document.write("Voce tem que se alistar imediatamente");
     } else if (idade < 18) {
-        document.write("Voce ainda não tem 18 anos, Ainda faltam ", +maiorDezoito+ " anos para se alistar");
+        document.write("Voce ainda não tem 18 anos, Ainda faltam ", +maiorDezoito + " anos para se alistar");
     } else if (idade > 18) {
-        document.write("Voce deveria ter se alistado há ", +menorDezeoito+ " anos")
+        document.write("Voce deveria ter se alistado há ", +menorDezeoito + " anos")
     }
 }
 alistamentoMilitar();
